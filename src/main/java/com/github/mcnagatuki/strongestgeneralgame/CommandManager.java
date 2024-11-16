1423e739c9a98c4796a6a3a32dde470896623c5e
McNagatuki <kikihiji.fuumi5447491@gmail.com>
2024-11-16 22:47:28 +0900

    fix: command

--- src/main/java/com/github/mcnagatuki/strongestgeneralgame/CommandManager.java
+++ src/main/java/com/github/mcnagatuki/strongestgeneralgame/CommandManager.java
@@ -3,28 +3,17 @@ package com.github.mcnagatuki.strongestgeneralgame;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import com.mojang.logging.LogUtils;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
-import net.minecraft.commands.arguments.TeamArgument;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.scores.PlayerTeam;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.slf4j.Logger;
-
-import java.util.function.Supplier;
 
 /**
- * - ready
+ * - initialize
  * - start
  * - stop
- * - ignore set <team>
- * - ignore delete
- * - ignore get
  */
 
 /*
@@ -34,40 +23,26 @@ import java.util.function.Supplier;
   sendSystemMessage(text) の場合: 送信者には、白文字で結果が出力。サーバーコンソールにはコマンド実行の表示なし。
  */
 
-
 @Mod.EventBusSubscriber(modid = StrongestGeneralGame.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.DEDICATED_SERVER)
 public class CommandManager {
-    private static final Logger LOGGER = LogUtils.getLogger();
-
-    public CommandManager() {
-    }
 
     @SubscribeEvent
     public static void onRegisterCommand(RegisterCommandsEvent event) {
         LiteralArgumentBuilder<CommandSourceStack> builder = Commands.literal("sgg")
-
-                .then(Commands.literal("ready")
-                        .executes(CommandManager::executeReady))
+                .then(Commands.literal("initialize")
+                        .executes(CommandManager::executeInitialize))
                 .then(Commands.literal("start")
                         .executes(CommandManager::executeStart))
                 .then(Commands.literal("stop")
-                        .executes(CommandManager::executeStop))
-                .then(Commands.literal("ignore")
-                        .then(Commands.literal("set")
-                                .then(Commands.argument("team", TeamArgument.team())
-                                        .executes(CommandManager::executeIgnoreSet)))
-                        .then(Commands.literal("del")
-                                .executes(CommandManager::executeIgnoreDelete))
-                        .then(Commands.literal("get")
-                                .executes(CommandManager::executeIgnoreGet))
+                        .executes(CommandManager::executeStop)
                 );
 
         event.getDispatcher().register(builder);
     }
 
-    public static int executeReady(CommandContext<CommandSourceStack> context) {
+    public static int executeInitialize(CommandContext<CommandSourceStack> context) {
         CommandSourceStack source = context.getSource();
-        MainLogic.setReady(source);
+        MainLogic.initializeGame(source);
         return Command.SINGLE_SUCCESS;
     }
 
@@ -80,41 +55,6 @@ public class CommandManager {
     public static int executeStop(CommandContext<CommandSourceStack> context) {
         CommandSourceStack source = context.getSource();
         MainLogic.endGame(source);
-        return Command.SINGLE_SUCCESS;
-    }
-
-    public static int executeIgnoreSet(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
-        PlayerTeam team = TeamArgument.getTeam(context, "team");
-        String teamName = team.getName();
-        MainLogic.setIgnoreTeam(teamName);
-
-        CommandSourceStack source = context.getSource();
-        String text = "[SGG] Team '" + teamName + "' will be ignored.";
-        Supplier<Component> supplier = () -> Component.literal(text);
-        source.sendSuccess(supplier, true);
-
-        return Command.SINGLE_SUCCESS;
-    }
-
-    public static int executeIgnoreDelete(CommandContext<CommandSourceStack> context) {
-        MainLogic.delIgnoreTeam();
-
-        CommandSourceStack source = context.getSource();
-        String text = "[SGG] Ignore team deleted.";
-        Supplier<Component> supplier = () -> Component.literal(text);
-        source.sendSuccess(supplier, true);
-
-        return Command.SINGLE_SUCCESS;
-    }
-
-    public static int executeIgnoreGet(CommandContext<CommandSourceStack> context) {
-        String ignoreTeamName = MainLogic.getIgnoreTeam();
-
-        CommandSourceStack source = context.getSource();
-        String text = "[SGG] Ignore team: '" + ignoreTeamName + "'";
-        Supplier<Component> supplier = () -> Component.literal(text);
-        source.sendSuccess(supplier, true);
-
         return Command.SINGLE_SUCCESS;
     }
 }
