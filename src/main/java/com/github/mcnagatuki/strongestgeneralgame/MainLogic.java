3314fa7cba262d7fb295bc3b6101b259ab455389
McNagatuki <kikihiji.fuumi5447491@gmail.com>
2024-11-23 06:57:59 +0900

    fix: behavior

--- src/main/java/com/github/mcnagatuki/strongestgeneralgame/MainLogic.java
+++ src/main/java/com/github/mcnagatuki/strongestgeneralgame/MainLogic.java
@@ -10,6 +10,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.GameType;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent.PlayerRespawnEvent;
 import net.minecraft.world.scores.Team;
 import net.minecraftforge.api.distmarker.Dist;
@@ -42,6 +43,16 @@ public class MainLogic {
     private static final String respawnTeamMember = respawnTpTagPrefix + "team_member";
     private static final String respawnRandomTp = respawnTpTagPrefix + "random";
 
+    // 遅延ゲームモード変更用タグ
+    private static final String tickEventTpTagPrefix = "sgg_tick_event_tp_";
+    private static final String tickEventTpDeathPoint = tickEventTpTagPrefix + "death_point";
+    private static final String tickEventTpTeamMember = tickEventTpTagPrefix + "team_member";
+    private static final String tickEventTpRandom = tickEventTpTagPrefix + "random";
+
+    // 遅延ゲームモード変更用タグ
+    private static final String lazyGameModeChangeTag = "sgg_lazy_game_mode_change";
+
+
     public MainLogic() {
     }
 
@@ -55,8 +66,14 @@ public class MainLogic {
         MinecraftServer server = source.getServer();
         TeamManager.initialize(server);
 
+        // ゲームモード遅延変更用の準備
+        LazyGameModeChanger.initialize(server);
+
         // 過去のダメージ記録を削除
         DamageTracker.clear();
+
+        // 過去のコマンドタグを削除
+        server.getPlayerList().getPlayers().forEach(MainLogic::clearTpGameModeChangeTag);
 
         // 準備完了
         Supplier<Component> supplier = () -> Component.literal("[SGG] Game is initialized.");
@@ -96,73 +113,6 @@ public class MainLogic {
             return;
         }
 
-        if (!TeamManager.isPlayerInGame(deathPlayer)) {
-            return;  // この時点で、プレイヤーはsgg_XXXのチームに入っていることが確定する。
-        }
-
-        // SGGプレイヤーの死亡時処理
-        if (TeamManager.isPlayerGeneral(deathPlayer)) {
-            // 死んだのが大将だった場合
-            executeDeathGeneral(deathPlayer);
-        } else if (TeamManager.isFallenWarrior(deathPlayer)) {
-            // 死んだのが落ち武者だった場合
-            executeDeathFallenWarrior(deathPlayer);
-        } else {
-            // 死んだのが一般プレイヤーだった場合
-            executeDeathCommonSoldier(deathPlayer);
-        }
-
-        // TODO: 相打ちの対処、自死（killerが自分になるケース）の対処
-    }
-
-    private static void executeDeathGeneral(@NotNull Player deathPlayer) {
-        // Serverのみで動作
-        MinecraftServer server = deathPlayer.getServer();
-        if (server == null) {
-            return;
-        }
-
-        // 死んだプレイヤーチーム・殺したプレイヤーチームの取得
-        Team deathPlayerTeam = deathPlayer.getTeam();
-        Optional<Team> killerTeam = getWhichTeamKillMe(deathPlayer);
-
-        // 死んだチームのメンバーを列挙
-        List<Player> deathPlayerTeamMembers = getTeamPlayers(server, deathPlayerTeam);
-
-        // 「大将が死にました」のアナウンス, MODではタグをつけるだけ
-        deathPlayerTeamMembers.forEach(e -> e.addTag(commandGeneralDeathAnnounceTag));
-
-        // 死因がSGGプレイヤーによるものではない場合
-        // あるいは、殺したチームが死んだチームと同じ場合（自殺？や相打ち）
-        if (killerTeam.isEmpty() || killerTeam.get().getName().equals(deathPlayerTeam.getName())) {
-            // チームの全プレイヤーを落ち武者化
-            TeamManager.moveTeamToFallenWarrior(server, deathPlayerTeam);
-
-            // 死んでいる人はランダムtp, tagで操作, 続きはonPlayerRespawn
-            deathPlayerTeamMembers
-                    .stream()
-                    .filter(e -> !e.isAlive())
-                    .forEach(e -> setRespawnTpTag(e, respawnRandomTp));
-            return;
-        }
-
-        // 一度全員スペクテイター化
-        deathPlayerTeamMembers.stream()
-                .filter(e -> e instanceof ServerPlayer)
-                .map(e -> (ServerPlayer) e)
-                .forEach(e -> e.setGameMode(GameType.SPECTATOR));
-
-        // 大将を殺したプレイヤーのチームにチーム変更
-        TeamManager.moveTeamToTeam(server, deathPlayerTeam, killerTeam.get());
-
-        // *** 以下の処理は、新チーム全員に適用 ***
-        getTeamPlayers(server, killerTeam.get())
-                .stream()
-                .filter(e -> !e.isAlive())
-                .forEach(e -> setRespawnTpTag(e, respawnTeamMember));
-    }
-
-    private static void executeDeathFallenWarrior(@NotNull Player deathPlayer) {
         // Serverのみで動作
         MinecraftServer server = deathPlayer.getServer();
         if (server == null) {
@@ -174,12 +124,100 @@ public class MainLogic {
             return;
         }
 
+        // sgg_XXXのチームに入っているプレイヤーのみ動作
+        if (!TeamManager.isPlayerInGame(deathPlayer)) {
+            return;
+        }
+
+        // クリエやスペクは対象外。サバイバルやアドベンチャーのみ。
+        if (deathServerPlayer.isSpectator() || deathServerPlayer.isCreative()) {
+            return;
+        }
+
+        // SGGプレイヤーの死亡時処理
+        if (TeamManager.isPlayerGeneral(deathServerPlayer)) {
+            // 死んだのが大将だった場合
+            executeDeathGeneral(server, deathServerPlayer);
+        } else if (TeamManager.isFallenWarrior(deathServerPlayer)) {
+            // 死んだのが落ち武者だった場合
+            executeDeathFallenWarrior(server, deathServerPlayer);
+        } else {
+            // 死んだのが一般プレイヤーだった場合
+            executeDeathCommonSoldier(server, deathServerPlayer);
+        }
+    }
+
+    private static void executeDeathGeneral(MinecraftServer server, ServerPlayer deathServerPlayer) {
+        // 死んだプレイヤーチーム・殺したプレイヤーチームの取得
+        Team deathPlayerTeam = deathServerPlayer.getTeam();
+        Optional<Team> killerTeam = getWhichTeamKillMe(deathServerPlayer);
+
+        // 死んだチームのメンバーを列挙
+        List<Player> deathPlayerTeamMembers = getTeamPlayers(server, deathPlayerTeam);
+
+        // 「大将が死にました」のアナウンス, MODではタグをつけるだけ
+        deathPlayerTeamMembers.forEach(e -> e.addTag(commandGeneralDeathAnnounceTag));
+
+        // 死んだチームの全員スペクテイター化
+        deathPlayerTeamMembers.stream()
+                .filter(e -> e instanceof ServerPlayer)
+                .map(e -> (ServerPlayer) e)
+                .forEach(e -> e.setGameMode(GameType.SPECTATOR));
+
+        /* 死因がSGGプレイヤーによるものではない場合
+           あるいは、殺したチームが死んだチームと同じ場合（自殺？や相打ち） */
+        if (killerTeam.isEmpty() || killerTeam.get().getName().equals(deathPlayerTeam.getName())) {
+            // チームの全プレイヤーを落ち武者化
+            TeamManager.moveTeamToFallenWarrior(server, deathPlayerTeam);
+
+            // TODO: サーバーにいないプレイヤーに対しては座標移動やゲームモード変更が効かない。要対応。
+            // ランダムtp, tagで操作, 続きはonPlayerTickEvent (生きているなら)
+            deathPlayerTeamMembers
+                    .stream()
+                    .filter(LivingEntity::isAlive)
+                    .forEach(e -> {
+                        setTpGameModeChangeTag(e, tickEventTpRandom, true);
+                    });
+
+            // ランダムtp, tagで操作, 続きはonPlayerRespawn (死んでいるなら)
+            deathPlayerTeamMembers
+                    .stream()
+                    .filter(e -> !e.isAlive())
+                    .forEach(e -> {
+                        setTpGameModeChangeTag(e, respawnRandomTp, true);
+                    });
+            return;
+        }
+
+        /* 以下、死因がSGGプレイヤーによる場合*/
+        // 大将を殺したプレイヤーのチームにチーム変更
+        TeamManager.moveTeamToTeam(server, deathPlayerTeam, killerTeam.get());
+
+        // TODO: サーバーにいないプレイヤーに対しては座標移動やゲームモード変更が効かない。要対応。
+        // *** 以下の処理は、新チーム全員に適用 ***
+        // チームメンバーtp, tagで操作, 続きはonPlayerTickEvent (生きている & スペクなら)
+        getTeamPlayers(server, killerTeam.get())
+                .stream()
+                .filter(LivingEntity::isAlive)
+                .filter(Player::isSpectator)
+                .forEach(e -> setTpGameModeChangeTag(e, tickEventTpTeamMember, true));
+
+
+        // チームメンバーtp, tagで操作, 続きはonPlayerRespawn (死んでいるなら)
+        getTeamPlayers(server, killerTeam.get())
+                .stream()
+                .filter(e -> !e.isAlive())
+                .forEach(e -> setTpGameModeChangeTag(e, respawnTeamMember, true));
+    }
+
+    private static void executeDeathFallenWarrior(MinecraftServer server, ServerPlayer deathServerPlayer) {
         // 殺したプレイヤーチームの取得
-        Optional<Team> killerTeam = getWhichTeamKillMe(deathPlayer);
-        // 死因がSGGプレイヤーによるものではない場合、または殺したプレイヤーが鯖にいない場合
+        Optional<Team> killerTeam = getWhichTeamKillMe(deathServerPlayer);
+
+        /* 死因がSGGプレイヤーによるものではない場合、または殺したプレイヤーが鯖にいない場合 */
         if (killerTeam.isEmpty()) {
             // ランダムtp, tagで操作, 続きはonPlayerRespawn
-            setRespawnTpTag(deathPlayer, respawnRandomTp);
+            setTpGameModeChangeTag(deathServerPlayer, respawnRandomTp, true);
             return;
         }
 
@@ -188,23 +226,18 @@ public class MainLogic {
         deathServerPlayer.setGameMode(GameType.SPECTATOR);
 
         // プレイヤーのチーム移動
-        TeamManager.addPlayerToTeam(deathPlayer, killerTeam.get());
+        TeamManager.addPlayerToTeam(deathServerPlayer, killerTeam.get());
 
         // 新チームのもとにリスポーン, tagで操作, 続きはonPlayerRespawn
-        setRespawnTpTag(deathPlayer, respawnTeamMember);
+        setTpGameModeChangeTag(deathServerPlayer, respawnTeamMember, true);
     }
 
-    private static void executeDeathCommonSoldier(@NotNull Player deathPlayer) {
-        // ServerPlayerのみで動作。すなわち、このMODはクライアントでは動かないはず。
-        if (!(deathPlayer instanceof ServerPlayer deathServerPlayer)) {
-            return;
-        }
-
+    private static void executeDeathCommonSoldier(MinecraftServer server, ServerPlayer deathServerPlayer) {
         // プレイヤーをスペクテイターにする
         deathServerPlayer.setGameMode(GameType.SPECTATOR);
 
         // 死んだ場所にリスポーン, tagで操作, 続きはonPlayerRespawn
-        setRespawnTpTag(deathPlayer, respawnDeathPoint);
+        setTpGameModeChangeTag(deathServerPlayer, respawnDeathPoint, false);
     }
 
     private static Optional<Team> getWhichTeamKillMe(Player deathPlayer) {
@@ -243,43 +276,51 @@ public class MainLogic {
                 .collect(Collectors.toList());
     }
 
-    public static List<Player> getTpAblePlayers(List<Player> players) {
-        // 生きている && ゲームモードサバイバル
-        return players.stream()
-                .filter(e -> e instanceof ServerPlayer)
-                .map(e -> (ServerPlayer) e)
-                .filter(LivingEntity::isAlive)
-                .filter(e -> !(e.isCreative() || e.isSpectator()))
-                .map(e -> (Player) e)
-                .toList();
-    }
-
-    public static <T> T getRandomElement(List<T> list) {
-        if (list.isEmpty()) {
-            return null;
+    public static void setTpGameModeChangeTag(Player player, String tagString, boolean lazyGameModeChange) {
+        // サーバーのみ動作
+        if (!(player instanceof ServerPlayer serverPlayer)) {
+            return;
         }
 
-        Random random = new Random();
-        int randomIndex = random.nextInt(list.size()); // 0 から list.size() - 1 の範囲でランダムなインデックスを生成
-        return list.get(randomIndex); // ランダムなインデックスの要素を返す
-    }
-
-    public static void setRespawnTpTag(Player player, String tagString) {
-        // 他のリスポーンタグをすべて削除
-        List<String> tags = player.getTags().stream().filter(e -> e.startsWith(respawnTpTagPrefix)).toList();
-        for (String tag : tags) {
-            player.removeTag(tag);
-        }
-
-        // コマンドランダムtp用タグも削除
-        player.removeTag(commandRandomTpTag);
+        // 残ってるタグをすべて削除
+        clearTpGameModeChangeTag(serverPlayer);
 
         // タグの追加
-        player.addTag(tagString);
+        serverPlayer.addTag(tagString);
+
+        // リスポーン後に遅延してゲームモード変更するか
+        if (lazyGameModeChange) {
+            serverPlayer.addTag(lazyGameModeChangeTag);
+        }
+    }
+
+    public static void clearTpGameModeChangeTag(ServerPlayer serverPlayer) {
+        // コマンドランダムtp用タグを削除
+        serverPlayer.removeTag(commandRandomTpTag);
+
+        // リスポーンタグを削除
+        List<String> respawnTags = serverPlayer.getTags().stream().filter(e -> e.startsWith(respawnTpTagPrefix)).toList();
+        for (String tag : respawnTags) {
+            serverPlayer.removeTag(tag);
+        }
+
+        // tick eventタグを削除
+        List<String> tickEventTags = serverPlayer.getTags().stream().filter(e -> e.startsWith(tickEventTpTagPrefix)).toList();
+        for (String tag : tickEventTags) {
+            serverPlayer.removeTag(tag);
+        }
+
+        // 遅延ゲームモード変更タグを削除
+        serverPlayer.removeTag(lazyGameModeChangeTag);
+
+        // 遅延ゲームモード変更の中止
+        LazyGameModeChanger.cancel(serverPlayer);
     }
 
     @SubscribeEvent
     public static void onPlayerRespawn(PlayerRespawnEvent event) {
+        /* 死んでいるプレイヤーに対して、リスポーン時に改めてチームメイトへの移動 & ゲームモードの変更 */
+
         // check if game is running
         if (!running) {
             return;
@@ -295,9 +336,10 @@ public class MainLogic {
             return;
         }
 
-        if (!serverPlayer.isAlive()) {
-            return;
-        }
+//        // TODO: これあっている？
+//        if (!serverPlayer.isAlive()) {
+//            return;
+//        }
 
         // リスポーンタグを取得
         Optional<String> respawnTag = serverPlayer.getTags().stream().filter(e -> e.startsWith(respawnTpTagPrefix)).findFirst();
@@ -305,53 +347,85 @@ public class MainLogic {
             return;
         }
 
+        // ゲームモード変更用のリスポーン設定
+        boolean isLazyGameModeChange = serverPlayer.getTags().contains(lazyGameModeChangeTag);
+
         // 他のリスポーンタグをすべて削除
-        List<String> respawnTags = serverPlayer.getTags().stream().filter(e -> e.startsWith(respawnTpTagPrefix)).toList();
-        respawnTags.forEach(serverPlayer::removeTag);
+        clearTpGameModeChangeTag(serverPlayer);
 
+        // メイン処理
         if (respawnTag.get().equals(respawnTeamMember)) {
-            // 新チームの名前を取得
-            Team team = serverPlayer.getTeam();
-            if (team == null) {
-                return;
-            }
-
-            // チームの中からリスポーンできる人を取得
-            List<Player> newTeamMembers = getTeamPlayers(server, team);
-            List<Player> tpCandidatePlayers = getTpAblePlayers(newTeamMembers);
-            Player tpDistPlayer = getRandomElement(tpCandidatePlayers);
-            if (tpDistPlayer == null) {
-                return;
-            }
-
-            // リス後にtp
-            ServerPlayer tpDistServerPlayer = (ServerPlayer) tpDistPlayer;
-            Vec3 pos = tpDistServerPlayer.position();
-            serverPlayer.teleportTo(pos.x, pos.y, pos.z);
-
+            TeleportManager.toTeamMember(serverPlayer);
         } else if (respawnTag.get().equals(respawnDeathPoint)) {
-            // 死んだ場所を取得
-            Optional<GlobalPos> deathLocation = serverPlayer.getLastDeathLocation();
-            if (deathLocation.isEmpty()) {
-                return;
-            }
-
-            // リス後にtp
-            Vec3 pos = deathLocation.get().pos().getCenter();
-            serverPlayer.teleportTo(pos.x, pos.y, pos.z);
-
+            TeleportManager.toDeathPoint(serverPlayer);
         } else if (respawnTag.get().equals(respawnRandomTp)){
             // ランダムtp, コマンドで実装
             serverPlayer.addTag(commandRandomTpTag);
-
         } else {
             // Do nothing
             return;
         }
 
-        // TODO: ゲームモード変更用のリスポーン設定
+        // ゲームモード変更用のリスポーン設定
+        if (isLazyGameModeChange) {
+            LazyGameModeChanger.register(serverPlayer, 5);
+        }
     }
 
+    @SubscribeEvent
+    public static void onPlayerTickEvent(TickEvent.PlayerTickEvent event) {
+        /* 既にリスポーン済みのプレイヤーに対して、改めてチームメイトへの移動 & ゲームモードの変更 */
+        // スペクテイター状態にあるプレイヤーは、onRespawnによる処理ではうまく対応できない。解決するべし。
+
+        // startのみ動作
+        if (event.phase != TickEvent.Phase.START) {
+            return;
+        }
+
+        // check if game is running
+        if (!running) {
+            return;
+        }
+
+        // サーバーのみで動作
+        if (!(event.player instanceof ServerPlayer serverPlayer)) {
+            return;
+        }
+
+        MinecraftServer server = serverPlayer.getServer();
+        if (server == null) {
+            return;
+        }
+
+        // tick event タグの取得
+        Optional<String> tickEventTag = serverPlayer.getTags().stream().filter(e -> e.startsWith(tickEventTpTagPrefix)).findFirst();
+        if (tickEventTag.isEmpty()) {
+            return;
+        }
+
+        // ゲームモード変更用のリスポーン設定
+        boolean isLazyGameModeChange = serverPlayer.getTags().contains(lazyGameModeChangeTag);
+
+        // 他のリスポーンタグをすべて削除
+        clearTpGameModeChangeTag(serverPlayer);
+
+        // メイン処理
+        if (tickEventTag.get().equals(tickEventTpTeamMember)) {
+            TeleportManager.toTeamMember(serverPlayer);
+        } else if (tickEventTag.get().equals(tickEventTpDeathPoint)) {
+            TeleportManager.toDeathPoint(serverPlayer);
+        } else if (tickEventTag.get().equals(tickEventTpRandom)) {
+            // ランダムtp, コマンドで実装
+            serverPlayer.addTag(commandRandomTpTag);
+        } else {
+            // Do nothing
+            return;
+        }
+
+        if (isLazyGameModeChange) {
+            LazyGameModeChanger.register(serverPlayer, 5);
+        }
+    }
 
     //    public static void gameRespawnPlayersAroundTeamMember(List<Player> players, Team targetTeam) {
 //        // たぶんカウントダウン処理が重くなるので、リスポーン処理はまとめてやった方がいい。
